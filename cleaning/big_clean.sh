a15ec5c0ec2448eb3f880f1429b9b967957b766a
Halley Paulson <paul1098@morris.umn.edu>
2020-08-30 19:48:49 -0500

    Completed task 2

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Lab 0 Clean Script
+
+name=$(mktemp -d goHereXXX)
+newname=$(basename $1 .tgz)
+tar xzf $1 -C $name
+
+grep -H -R "DELETE ME!" $name | cut -d: -f1 | xargs rm
+here=$(pwd)
+cd $name
+
+tar -czf $here/cleaned_$newname.tgz $newname 
+
+
+
